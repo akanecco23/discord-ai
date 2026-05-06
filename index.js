e29fecddf1c0cba8f18068438a96b7816d759dd2
Akane Kurokawa <akanecco23@outlook.com>
2026-05-05 21:44:06 -0700

    clean outputs

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ client.on(Events.MessageCreate, async (message) => {
 			conversations[cId] = conversations[cId].slice(-100);
 		}
 
-		await reply.edit(response);
+		await reply.edit(response.replaceAll("*", "\\*").replaceAll("_", "\\_"));
 	} catch (e) {
 		console.error(e);
 		try {
